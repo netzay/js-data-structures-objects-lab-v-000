6a097c4c05e9dd3877eb9d0ef7de071c733b9340
Ally Netzel <netzelally@gmail.com>
2018-11-23 22:26:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ function  updateDriverWithKeyAndValue(obj, key, value) {
 
 function destructivelyUpdateDriverWithKeyAndValue(obj, key, value){
   
-  driver[key] = value;
-  return driver
+  return Object.assign(driver, { [key]: value });
+
 }
 
 function deleteFromDriverByKey(driver, key){
